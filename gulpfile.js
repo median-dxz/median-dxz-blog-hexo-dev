15786e04e1b7322ab1e32a72f7f04cb12f6d1e8a
median-dxz <med_iridescence@163.com>
2018-11-25 16:32:33 +0800

    添加高版本html-minifier,更新背景，更新字库，更新js类库

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,37 @@
 var gulp = require('gulp');
-var minifycss = require('gulp-minify-css');
+var minifycss = require('gulp-clean-css');
 var uglify = require('gulp-uglify');
 var htmlmin = require('gulp-htmlmin');
 var htmlclean = require('gulp-htmlclean');
-// 压缩 public 目录 css
+// 压缩css文件
 gulp.task('minify-css', function() {
-    return gulp.src('./public/**/*.css')
-        .pipe(minifycss())
-        .pipe(gulp.dest('./public'));
+  return gulp.src('./public/**/*.css')
+  .pipe(minifycss())
+  .pipe(gulp.dest('./public'));
 });
-// 压缩 public 目录 html
+// 压缩js文件
+gulp.task('minify-js', function() {
+  return gulp.src('./public/**/*.js')
+  .pipe(uglify())
+  .pipe(gulp.dest('./public'));
+});
+// 压缩html文件
 gulp.task('minify-html', function() {
   return gulp.src('./public/**/*.html')
-    .pipe(htmlclean())
-    .pipe(htmlmin({
-         removeComments: true,
-         minifyJS: true,
-         minifyCSS: true,
-         minifyURLs: true,
-    }))
-    .pipe(gulp.dest('./public'))
+  .pipe(htmlclean())
+  .pipe(htmlmin({
+    removeComments: true,//清除HTML注释
+    collapseWhitespace: true,//压缩HTML
+    collapseBooleanAttributes: true,//省略布尔属性的值 <input checked="true"/> ==> <input />
+    removeEmptyAttributes: true,//删除所有空格作属性值 <input id="" /> ==> <input />
+    removeScriptTypeAttributes: true,//删除<script>的type="text/javascript"
+    removeStyleLinkTypeAttributes: true,//删除<style>和<link>的type="text/css"
+    minifyJS: true,//压缩页面JS
+    minifyCSS: true//压缩页面CSS
+  }))
+  .pipe(gulp.dest('./public'))
 });
-// 压缩 public/js 目录 js
-gulp.task('minify-js', function() {
-    return gulp.src('./public/**/*.js')
-        .pipe(uglify())
-        .pipe(gulp.dest('./public'));
-});
-// 执行 gulp 命令时执行的任务
+// 默认任务
 gulp.task('default', [
-    'minify-html','minify-css','minify-js'
+  'minify-css','minify-js'
 ]);
