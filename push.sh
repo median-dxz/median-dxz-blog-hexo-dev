89e8459681a8bd79b74c1d2c73e1dc5bd5f8c55e
median-dxz <med_iridescence@163.com>
2018-12-21 18:21:32 +0800

    updata weekly

--- push.sh
+++ push.sh
@@ -1,1 +1,1 @@
-hexo clean && hexo g && node htmlformat.js && hexo d && git add . && git commit -m "updata weekly" && git push
+hexo clean && hexo g && hexo img && node htmlformat.js && hexo d && git add . && git commit -m "updata weekly" && git push
