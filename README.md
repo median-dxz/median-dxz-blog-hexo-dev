1f5dde0f18c642cdd9de7e30b7f0c61dffd2f264
median-dxz <med_iridescence@163.com>
2021-03-20 00:30:37 +0800

    meting换上自部署

--- README.md
+++ README.md
@@ -7,14 +7,18 @@ Median-dxz's Blog Project
 - 2021/3/13 跟进项目中
 - 2021/3/14 重构音乐获取部分代码
 - 2021/3/15 解决使用pajx导致的局部js失效
+- 2021/3/20 将meting部署到自己的vercel上，迁移api
 
 # 当前
 
 - [x] music实现async和await
 - [x] 解决页面pajx的局部js处理
+- [x] 将meting部署到vercel上
 - [ ] hito自定义对齐方式
 - [ ] 无图片时的页脚样式
 - [ ] about:头图显示pid和作者，跳转主站并在跳转主站的地方使用webpack环境变量
+- [ ] about:img favicon
+- [ ] about footer时间
 
 # TODO
 
--- source/assets/js/music/getMusic.js
+++ source/assets/js/music/getMusic.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 async function getMusicRepAsync(songid, plat) {
-    var url = 'https://api.i-meto.com/meting/api?server=%plat%&type=song&id=%id%&r=song';
+    var url = 'https://meting-med.vercel.app/api?server=%plat%&type=song&id=%id%&r=song';
     url = url.replace('%id%', String(songid)).replace('%plat%', plat);
     return $.getJSON(url).promise();
 }
